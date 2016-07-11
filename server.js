d66c0931e4114c04004ad6489b72f6b64af0342d
Dax <limeside@gmail.com>
2016-07-11 14:00:14 -0500

    Add more setup. :rabbit:

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
+    index = require('./routes/index');
+
+// === Middleware === //
+app.use(bodyParser.json())
+  .use(bodyParser.urlencoded({extended:false}))
+  .use(methodOverride('_method'))
+  .use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs')
+  .set('views', __dirname + '/views');
+
+
+// === Routes === //
+app.use('/', index);
+
+
+// === Listen === //
+app.listen(3000, function(){
+  console.log('Server is listening.');
+});
