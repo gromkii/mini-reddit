dabde2ddf519e7e886895799382df3db43db73b5
Dax <limeside@gmail.com>
2016-07-12 11:41:07 -0500

    Add working templating on index. :rabbit:

--- public/app.js
+++ public/app.js
@@ -1,5 +1,29 @@
-var app = angular.module('miniReddit');
+var app = angular.module('miniReddit', ['ngRoute']);
 
 app.controller('IndexController', function(){
   this.greeting = "What's up.";
 });
+
+app.config(function($locationProvider, $routeProvider){
+  $routeProvider
+  .when('/', {
+    templateUrl: 'views/index.html',
+    controller: 'IndexController',
+    controllerAs: 'index'
+  })
+
+  .when('/users',{
+    templateUrl: 'views/users/index.html',
+    controller: 'UsersController',
+    controllerAs: 'users'
+  });
+
+  $locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false
+  });
+})
+
+app.controller('UsersController', ['$http', function($http){
+
+}]);
