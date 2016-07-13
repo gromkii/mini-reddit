b6a2f8279129d0cf2320f7a080241f70b975020f
Dax <limeside@gmail.com>
2016-07-13 16:42:26 -0500

    Add ngAnimate, will experiment later. :rabbit:

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('miniReddit', ['ngRoute']);
+var app = angular.module('miniReddit', ['ngRoute','ngAnimate']);
 
 app.controller('IndexController', function(){
   this.greeting = "What's up.";
